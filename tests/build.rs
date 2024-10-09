de1b7de1a6d5f427223ebb62279155f6f29f8b09
Aaron Loyd <aloyd@cloudflare.com>
2024-10-09 15:47:42 -0500

    Refactor tests build.rs to support multiple sets of schemas
    This will be used to test the missing node fix.

--- tests/build.rs
+++ tests/build.rs
@@ -1,9 +1,20 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     println!("cargo::rerun-if-changed=schemas");
 
-    recapnc::CapnpCommand::new()
-        .src_prefix("schemas")
-        .file("schemas/build.capnp")
-        .file("schemas/bar.capnp")
-        .write_to_out_dir();
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    macro_rules! run {
+        ($base:literal: $($path:literal),*) => {
+            recapnc::CapnpCommand::new()
+                .src_prefix(concat!("schemas/", $base))
+                $(.file(concat!("schemas/", $base, "/", $path)))*
+                .write_to(out_dir.join($base))
+        };
+    }
+
+    // Simple test with imports
+    run!("build_rs": "build.capnp", "bar.capnp");
 }
